a1a2729c65d87b5e7d9f0717f1f5161145cf588b
Markus Waas <mail@markuswaas.com>
2024-08-13 12:46:13 +0200

    chore: update dependencies for inj version

--- packages/utils/src/lib.rs
+++ packages/utils/src/lib.rs
@@ -1,5 +1,3 @@
 mod curve;
-mod scalable_curve;
 
 pub use curve::{Curve, CurveError, PiecewiseLinear, SaturatingLinear};
-pub use scalable_curve::{ScalableCurve, ScalableLinear, ScalablePiecewise};
